abe923ffc4211f2583ff2ac16c193eaf0a634424
rossy <rossygibb@gmail.com>
2025-12-04 13:13:45 +0000

    Demo update for CI/CD recording (v4)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Weather ML App</h1><p>Deployed via CI/CD pipeline.</p>"
+    return "<h1>Weather ML App</h1><p>Deployed via CI/CD pipeline.By the great group 11 !</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
